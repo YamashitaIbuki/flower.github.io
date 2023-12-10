94cf4481278c62fcbfc3f150dec4928cb7b77ef4
flower <ibuki_milktea@yahoo.co.jp>
2023-12-10 09:07:42 +0900

    12/10 commit

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 let aa = $('#hoge');
 
 $(() => {
-    console.log('goe');
+    click_scloll();
 })
+
+function click_scloll() {
+    console.log('-click_scloll-');
+
+    $('.navbar-brand').on('click', function() {
+        let id = $(this).attr('href');
+        let pos = $(id).offset().top - 60;
+        $('html,body').animate({'scrollTop': pos },0);
+        return false;
+    });
+}
